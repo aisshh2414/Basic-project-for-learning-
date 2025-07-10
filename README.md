a576a287f445fe1c2e721a62748fcb1db568c63b
aisshh2414 <aishupkm@gmail.com>
2025-07-10 17:59:12 +0530

    Update and rename README.md to Zepto_SQL_data_analysis.sql

--- README.md
+++
@@ -1,1 +0,0 @@
-# Basic-project-for-learning-
---
+++ Zepto_SQL_data_analysis.sql
@@ -0,0 +1,3 @@
+📦 Zepto Inventory SQL Project
+
+This is a basic SQL project inspired by a Zepto-style inventory management system. It's designed to help me learn SQL fundamentals such as data modeling, writing queries, data analysis, and generating insights using real-world-like datasets.
